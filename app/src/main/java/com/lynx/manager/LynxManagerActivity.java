6d7f704f0790759d568f2cf6c36aea3a4d1b998d
aliakbar6821 <1aliakbar6821@gmail.com>
2026-04-24 16:50:54 +0500

    Fix: Force custom theme in Activity to prevent InflateException

--- app/src/main/java/com/lynx/manager/LynxManagerActivity.java
+++ app/src/main/java/com/lynx/manager/LynxManagerActivity.java
@@ -6,12 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 public class LynxManagerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_LynxManager);
         super.onCreate(savedInstanceState);
         if (savedInstanceState == null) {
             getSupportFragmentManager()
-                .beginTransaction()
-                .replace(android.R.id.content, new LynxPreferenceFragment())
-                .commit();
+                    .beginTransaction()
+                    .replace(android.R.id.content, new LynxPreferenceFragment())
+                    .commit();
         }
     }
 }
